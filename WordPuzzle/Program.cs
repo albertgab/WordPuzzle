58d594b89428b31a17305afce3d12ddc5a750102
Albert Gabrychowicz <albertg980@gmail.com>
2021-03-28 16:26:51 +0100

    Second Commit

---
+++ WordPuzzle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WordPuzzle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
