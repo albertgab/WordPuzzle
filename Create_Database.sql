b798063a11c684839895d3a3f034b69dd2e14294
Albert Gabrychowicz <albertg980@gmail.com>
2021-03-28 21:02:35 +0100

    Creating and populating database

---
+++ Create_Database.sql
@@ -0,0 +1,47 @@
+﻿DROP TABLE History;
+DROP TABLE Solutions;
+DROP TABLE Users;
+DROP TABLE Levels;
+
+
+CREATE TABLE Users(
+UserID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+Username VARCHAR (20) NOT NULL CHECK (LEN(Username) > 4),
+Password VARCHAR (30) NOT NULL CHECK (LEN(Password) > 5),
+Score INT NOT NULL DEFAULT 0
+);
+
+CREATE TABLE History(
+HistoryID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+UserID INT NOT NULL CHECK (UserID > 0),
+LevelID INT NOT NULL CHECK (LevelID > 0),
+Time TIME NOT NULL,
+Score INT NOT NULL
+);
+
+CREATE TABLE Levels(
+LevelID  INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+Name VARCHAR (30) NOT NULL, 
+Size INT NOT NULL  CHECK (Size >= 1010 AND Size <= 8989),  --first 2 digits are horizontal size, next 2 are vertical
+Letters VARCHAR (MAX) NOT NULL    --(LEN(Letters) = (Size%100) * (Size-(Size%100)/100))
+);
+
+CREATE TABLE Solutions(
+LevelID INT NOT NULL,
+Word VARCHAR (20) NOT NULL
+PRIMARY KEY (LevelID, Word)
+);
+
+ALTER TABLE History
+ADD CONSTRAINT FK_History_Users
+FOREIGN KEY (UserID) REFERENCES Users(UserID);
+
+ALTER TABLE History
+ADD CONSTRAINT FK_History_Levels
+FOREIGN KEY (LevelID) REFERENCES Levels(LevelID);
+
+ALTER TABLE Solutions
+ADD CONSTRAINT FK_Solutions_Levels
+FOREIGN KEY (LevelID) REFERENCES Levels(LevelID);
+
+SELECT * FROM Levels
---
+++ Populate_Database.sql
@@ -0,0 +1,160 @@
+﻿INSERT INTO [Users]
+           ([Username]
+           ,[Password]
+           )
+     VALUES
+	 		     ('Leonidas','Sparta123')
+;
+
+INSERT INTO [Users]
+           ([Username]
+           ,[Password]
+           )
+     VALUES
+	 		     ('Boomer','qwe321')
+;
+
+INSERT INTO [Levels]
+           ([Name]
+           ,[Size]
+           ,[Letters]
+           )
+     VALUES
+	 		     ('Animals', 1010, 'JSDOIVBPOA' +
+                                   'ASDGMONKEY' +
+                                   'UXZYUIVTOS' +
+                                   'KJTGVBLWSE' +
+                                   'IGIRAFFESH' +
+                                   'DSGKUIVGHE' +
+                                   'IZEBRAVBOI' +
+                                   'PWRIEVUOZW' +
+                                   'XIPUCHZXPE' +
+                                   'BELKUIVGSR')
+;
+
+INSERT INTO [Levels]
+           ([Name]
+           ,[Size]
+           ,[Letters]
+           )
+     VALUES
+	 		     ('Food', 1010, 'SFGHDFHDFH' +
+                                'NUGGETSTRN' +
+                                'TAEHIVPLCE' +
+                                'VZXOIUAEWE' +
+                                'ZBUTBNGHEO' +
+                                'OUIDHBHDOI' +
+                                'VNOODLESVR' +
+                                'AWEGUVTDFV' +
+                                'ZCURRYTBSE' +
+                                'ERSLIPIZZA')
+;
+
+INSERT INTO [Solutions]
+           ([LevelID]
+           ,[Word]
+           )
+     VALUES
+	 		     (2,'PIZZA')
+;
+INSERT INTO [Solutions]
+           ([LevelID]
+           ,[Word]
+           )
+     VALUES
+	 		     (2,'SPAGHETTI')
+;
+INSERT INTO [Solutions]
+           ([LevelID]
+           ,[Word]
+           )
+     VALUES
+	 		     (2,'NOODLES')
+;
+INSERT INTO [Solutions]
+           ([LevelID]
+           ,[Word]
+           )
+     VALUES
+	 		     (2,'HOTDOG')
+;
+INSERT INTO [Solutions]
+           ([LevelID]
+           ,[Word]
+           )
+     VALUES
+	 		     (2,'CURRY')
+;
+INSERT INTO [Solutions]
+           ([LevelID]
+           ,[Word]
+           )
+     VALUES
+	 		     (2,'NUGGETS')
+;
+
+INSERT INTO [Solutions]
+           ([LevelID]
+           ,[Word]
+           )
+     VALUES
+	 		     (1,'BULL')
+;
+INSERT INTO [Solutions]
+           ([LevelID]
+           ,[Word]
+           )
+     VALUES
+	 		     (1,'ZEBRA')
+;
+INSERT INTO [Solutions]
+           ([LevelID]
+           ,[Word]
+           )
+     VALUES
+	 		     (1,'TIGER')
+;
+INSERT INTO [Solutions]
+           ([LevelID]
+           ,[Word]
+           )
+     VALUES
+	 		     (1,'GIRAFFE')
+;
+INSERT INTO [Solutions]
+           ([LevelID]
+           ,[Word]
+           )
+     VALUES
+	 		     (1,'MONKEY')
+;
+
+INSERT INTO [History]
+           ([UserID]
+           ,[LevelID]
+           ,[Time]
+           ,[Score]
+           )
+     VALUES
+	 		     (2,1, '00:05:34', 354)
+;
+
+INSERT INTO [History]
+           ([UserID]
+           ,[LevelID]
+           ,[Time]
+           ,[Score]
+           )
+     VALUES
+	 		     (2,2, '00:07:54', 257)
+;
+
+INSERT INTO [History]
+           ([UserID]
+           ,[LevelID]
+           ,[Time]
+           ,[Score]
+           )
+     VALUES
+	 		     (2,2, '00:03:29', 423)
+;
