96c64140df402a202f89af86daffe4d972f6c080
Albert Gabrychowicz <albertg980@gmail.com>
2021-04-05 21:16:44 +0100

    Class Diagrams added

--- WordPuzzleWPF/obj/Debug/net5.0-windows/LoginUC.g.i.cs
+++ WordPuzzleWPF/obj/Debug/net5.0-windows/LoginUC.g.i.cs
@@ -70,7 +70,7 @@ namespace WordPuzzleWPF {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WordPuzzleWPF;component/loginuc.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WordPuzzleWPF;V1.0.0.0;component/loginuc.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\LoginUC.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- WordPuzzleWPF/obj/Debug/net5.0-windows/MainWindow.g.i.cs
+++ WordPuzzleWPF/obj/Debug/net5.0-windows/MainWindow.g.i.cs
@@ -86,7 +86,7 @@ namespace WordPuzzleWPF {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WordPuzzleWPF;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WordPuzzleWPF;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
